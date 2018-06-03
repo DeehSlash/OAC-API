ea0daa9dc7698a3487d1e424e376db25307a5096
André Luiz <andreluiz.97@outlook.com>
2018-06-03 00:07:58 -0300

    Adicionado headers à todas requests

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Requested-With, Accept')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
+
 app.use('/', routes)
 
 networkService.extendPrototype()
