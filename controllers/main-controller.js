ea44ea997fb33ad609f8e8f62e121423b6e022e7
André Luiz <andreluiz.97@outlook.com>
2018-06-02 23:15:18 -0300

    Implementado método para criar a rede, treinar e guardar no cache

--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -1,6 +1,16 @@
 // Modules
 const cache = require('memory-cache')
 const randomstring = require('randomstring')
+const synaptic = require('synaptic')
+
+const Neuron    =   synaptic.Neuron,
+    	Layer     =   synaptic.Layer,
+	    Network   =   synaptic.Network,
+	    Trainer   =   synaptic.Trainer,
+      Architect =   synaptic.Architect
+
+// Services
+const networkService = require('../services/network-service')
 
 
 /**
@@ -10,8 +20,21 @@ const randomstring = require('randomstring')
  */
 const createNetwork = data => {
   return new Promise((resolve, reject) => {
+    // Generates a new code
     let code = generateCode()
 
+    // Creates the network and the trainer
+    const perceptron = new networkService.Perceptron(1, 5, 4)
+    const trainer = new Trainer(perceptron)
+
+    // Trains the network
+    trainer.train(data)
+
+    // Stores the network and the trainer
+    cache.put(`${code}/network`, perceptron)
+    cache.put(`${code}/trainer`, trainer)
+
+    // Resolve the promise returning the code
     resolve(code)
   })
 }
