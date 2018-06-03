74f8bd48b87b36b2a3c36d94b250a173b60535aa
André Luiz <andreluiz.97@outlook.com>
2018-06-02 23:05:06 -0300

    Adicionado synaptic às dependências, criado network service com protótipo de Perceptron

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const routes = require('./routes/index')
 
 const app = express()
 
+const networkService = require('./services/network-service')
+
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use('/', routes)
+
+networkService.extendPrototype()
 
 module.exports = app
---
+++ services/network-service.js
@@ -0,0 +1,36 @@
+/**
+ * Perceptron constructor
+ * @param {number} input Number of neurons in the input layer
+ * @param {number} hidden Number of neurons in the hidden layer
+ * @param {number} output Number of neurons in the output layer
+ */
+const Perceptron = (input, hidden, output) => {
+	// Create the layers
+	var inputLayer = new Layer(input)
+	var hiddenLayer = new Layer(hidden)
+	var outputLayer = new Layer(output)
+
+	// Connect the layers
+	inputLayer.project(hiddenLayer)
+	hiddenLayer.project(outputLayer)
+
+	// Set the layers
+	this.set({
+		input: inputLayer,
+		hidden: [hiddenLayer],
+		output: outputLayer
+	})
+}
+
+/**
+ * Extends the Perceptron prototype
+ */
+const extendPrototype = () => {
+  Perceptron.prototype = new Network()
+  Perceptron.prototype.constructor = Perceptron
+}
+
+module.exports = {
+  Perceptron,
+  extendPrototype
+}
