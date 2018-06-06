869f5001be0bc384838ede217d86a287e252411e
André Luiz <andreluiz.97@outlook.com>
2018-06-06 13:39:07 -0300

    Correções

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Requested-With, Accept')
   res.setHeader('Access-Control-Allow-Credentials', true)
